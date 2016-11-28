0d337f314cd4ffd5f9e31440b265fb494d7e58f3
Jonas <jonas.nolife@gmail.com>
2016-11-28 17:40:23 +0000

    s

--- src/main/scala/RX.scala
+++ src/main/scala/RX.scala
@@ -1,23 +1,29 @@
+import akka.http.scaladsl.model.{ContentType, HttpEntity, HttpResponse, MediaTypes}
 import akka.http.scaladsl.server.{Directives, Route}
 
 class RX extends Directives with JsonSupport {
   val route = {
     logRequestResult("disease-report-rx") {
-      get {
-        pathPrefix("disease") {
+      path("disease") {
+        get {
           complete(DiseaseReport("patientId","diseaseId",0,0,0))
+        } ~ post {
+          entity(as[DiseaseReport]) { diseaseReport =>
+            complete {
+              diseaseReport
+            }
+          }
         }
-      } ~ get {
-        pathPrefix("symptom") {
+      } ~ path("symptom") {
+        get {
           complete(SymptomReport("patientId","symptomId",0,0,0))
-        }
-      } ~ post {
-        entity(as[DiseaseReport]) { diseaseReport =>
-          complete(s"Stored disease")
-        }
-      } ~ post {
-        entity(as[SymptomReport]) { symptomReport =>
-          complete(s"Stored disease")
+        } ~ post {
+          entity(as[SymptomReport]) { symptomReport =>
+//            complete(s"Stored symptom " + symptomReport)
+              complete {
+                symptomReport
+              }
+          }
         }
       }
     }
